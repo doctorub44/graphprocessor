b44012c1fc74c044c01face022ee9be320bb14be
doctorub44 <david.uhrlaub@gmail.com>
2022-12-11 17:03:36 -0600

    added tests

--- graphproc/graphrule_test.go
+++ graphproc/graphrule_test.go
@@ -9,6 +9,20 @@ func TestRule(t *testing.T) {
 	args := map[string]any{"a": 5}
 	result, err := cond.Rule(args)
 	if !result || err != nil {
-		fatalError(t, nil, "Unable to evaluate rule")
+		fatalError(t, nil, "Unable to evaluate integer rule")
+	}
+
+	cond2 := NewCondition("a == `peterpiper`")
+	args2 := map[string]any{"a": "peterpiper"}
+	result2, err2 := cond2.Rule(args2)
+	if !result2 || err2 != nil {
+		fatalError(t, nil, "Unable to evaluate string rule")
+	}
+
+	cond3 := NewCondition("a == `peterpiper` && b == `paid the boss`")
+	args3 := map[string]any{"a": "peterpiper", "b": "paid the boss"}
+	result3, err3 := cond3.Rule(args3)
+	if !result3 || err3 != nil {
+		fatalError(t, nil, "Unable to evaluate string rule")
 	}
 }
